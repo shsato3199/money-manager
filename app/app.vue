7ec9286bdb9f74512b5a5567cf9b22d639b9bf36
shsato3199 <130293292+shsato3199@users.noreply.github.com>
2026-08-06 23:40:22 +0900

    フォーマッターの導入

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,6 @@
   <div class="container py-5">
     <h2 class="mb-4">Bootstrap表示確認</h2>
 
-<button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-primary">Primary</button>
   </div>
 </template>
